6a92e8b84384a3a39e9c288441d5b9b37c6087e7
minha723 <minhakim0723@naver.com>
2022-07-29 08:45:19 +0900

    initial commit

---
+++ src/main/java/com/its/re_test_project/ReTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.its.re_test_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReTestProjectApplication.class, args);
+    }
+
+}
